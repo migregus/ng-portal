72770d9ae2b628e19f5b0fa1242c7ccad6d6b945
migregus@gmail.com <mol10nka>
2017-11-22 09:44:28 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign/sign-in.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
